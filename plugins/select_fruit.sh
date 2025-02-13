42370edf38cee89a86dfd2e55c8e423cfb4cac8c
shye0167 <ktsh0167@gmail.com>
2025-02-13 14:53:43 +0900

    plugins 폴더추가

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,11 @@
+FRUIT=$1
+
+if [ $FRUIT == APPLE ];then
+	echo "you selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+        echo "you selected Orange!"
+elif [ $FRUIT == GRAPE ];then
+        echo "you selected Graoe!"
+else
+	echo "you selected other Fruits!"	
+fi	
