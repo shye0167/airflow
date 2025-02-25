5f2af29873117eac1dbde9c3665cededa6e5b133
unknown <ktsh0167@gmail.com>
2025-02-25 15:02:33 +0900

    op_args 테스트용 dag 업로드

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_with_op_args",
     schedule="30 6 * * * ",
     start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
     catchup=False,
